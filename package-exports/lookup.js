875f9df07d69d94b832ec5d28633316407c115b8
naugtur <naugtur@gmail.com>
2022-04-07 12:17:58 +0200

    lookup depth

--- package-exports/lookup.js
+++ package-exports/lookup.js
@@ -1,13 +1,15 @@
 const data = require("./data.json");
-
+const util = require("util");
 const search = process.argv[2];
 if (!search) {
   console.log(`usage: node lookup packagenames`);
   process.exit(1);
 }
-const lookup = search.split(",").map(a=>a.trim());
+const lookup = search.split(",").map((a) => a.trim());
 function print({ name, exports }) {
   console.log(name, "------------");
-  console.log(exports);
+  console.log(
+    util.inspect(exports, { showHidden: false, depth: Infinity, colors: true })
+  );
 }
 data.filter(({ name }) => lookup.includes(name)).map(print);
