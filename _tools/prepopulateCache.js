6bf42b9f68b91151e18feda7ce9ff0a0f3a9aef8
naugtur <naugtur@gmail.com>
2025-10-20 10:32:45 +0200

    prepopulateCache counts seed

--- _tools/prepopulateCache.js
+++ _tools/prepopulateCache.js
@@ -2,6 +2,8 @@ const { recursiveScan } = require("./npmscanner");
 const seed = require("../popular-pkgs/names-per-dependents_count.json");
 const path = require("path");
 
+console.log(`Seeding with ${seed.length} packages.`);
+
 return recursiveScan({
   targetPath: path.join(__dirname, "../_cache"),
   name: "cache",
